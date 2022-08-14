ea48ac054b20cc02b73de61f648ae072acdb89eb
carterwsheppard <carterwsheppard@gmail.com>
2022-08-14 19:35:01 -0400

    HW for Social API

---
+++ models/User.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+  
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+
+    friends: [
+   
+    ]
+
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
+
+PizzaSchema.virtual('friendCount').get(function() {
+  return this.friends.reduce(
+    (total, friend) => total + friend.length + 1,
+    0
+  );
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
